7f95361d6c533cd3b538778215565dc11dd2c882
Johan Vandingenen <vandingenen.johan@gmail.com>
2021-10-24 10:33:34 +0200

    multilanguage

--- src/i18n.js
+++ src/i18n.js
@@ -9,20 +9,19 @@ i18n
     lng: 'en',
     backend: {
       /* translation file path */
-      loadPath: '/assets/i18n/{{lng}}.json'
+      loadPath: '/assets/i18n/{{lng}}.json',
+      allowMultiLoading: true
     },
     fallbackLng: 'en',
     debug: true,
-    /* can have multiple namespace, in case you want to divide a huge translation into smaller pieces and load them on demand */
-    ns: ['translations'],
-    defaultNS: 'translations',
     keySeparator: false,
     interpolation: {
       escapeValue: false,
       formatSeparator: ','
     },
     react: {
-      wait: true
+      wait: true,
+      useSuspense: true
     }
   })
 
