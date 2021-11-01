7e3dc6123e6d7599467658457a58a1929f561ac9
Johan Vandingenen <vandingenen.johan@gmail.com>
2021-11-01 09:36:44 +0100

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
-import React, {Suspense} from 'react';
+import React, { Suspense } from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+import ReactGA from 'react-ga';
 import Introduction from './components/introduction/Introduction';
+
+const history = createHistory()
+ReactGA.initialize('G-1BK09BE5RZ');
+history.listen((location, action) => {
+  ReactGA.pageview(location.pathname + location.search);
+  console.log(location.pathname)
+});
+
 
 function App() {
   return (
     <Suspense fallback={"loading"}>
-      <div>
-        <Introduction/>
-      </div>
-  </Suspense>
+      <Router history={history}>
+        <div className="App">
+
+          <Switch>
+            <Route exact path="/" component={Introduction} />
+          </Switch>
+
+        </div>
+      </Router>
+    </Suspense>
   );
 }
 
